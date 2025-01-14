d0666a0cb6f121aeebb8023d2e561a3e5bded4b8
SleepyHeadWizard <shiwaditya.chandra@gmail.com>
2025-01-14 09:15:53 +0530

    Add index.php with basic PHP examples for greeting, odd/even check, and comparison

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$name = "John";
+echo "<h1 style='color: red;'>Hello $name</h1>";
+?>
+
+<?php #number is ODD or EVEN
+$number = 7;
+if ($number/2 == 0) {
+    echo "Number is even";
+} else {
+    echo "Number is odd";
+}
+?>
+
+<?php #PROGRAM to check grater
+$a = 10;
+$b = 20;
+if ($a > $b) {
+    echo "A is greater";
+} else {
+    echo "B is greater";
+}
+?>
