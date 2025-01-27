0857828443c188f41d4f9939d958312a9416dab0
SleepyHeadWizard <shiwaditya.chandra@gmail.com>
2025-01-27 09:21:48 +0530

    Add assignment operations in assingment.php for basic arithmetic calculations

---
+++ assingment.php
@@ -0,0 +1,10 @@
+<?php
+$a = 10;
+$b = 20;
+
+echo 'a+b = '.($a+=$b).'<br>';
+echo 'a-b = '.($a-=$b).'<br>';
+echo 'a*b = '.($a*=$b).'<br>';
+echo 'a/b = '.($a/=$b).'<br>';
+echo 'a%b = '.($a%=$b);
+?>
